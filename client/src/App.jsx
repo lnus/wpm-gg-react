6e8869d64697b9a4636be8304cc32b5b8b6559fb
Linus Wæhler <linus.wahler@gmail.com>
2021-03-21 00:59:56 +0100

    Refactor 2.0

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+import WordDisplay from './Components/WordDisplay/WordDisplay';
+
+const App = () => {
+  return (
+    <div>
+      <WordDisplay />
+    </div>
+  );
+};
+
+export default App;
